f7f5d686606abff0c44424e9b93bab34dcff13d6
aakarshan <aakartionsimkhada@gmail.com>
2024-02-02 12:38:00 -0600

    Lab8 is created

---
+++ React/lab8/lab8/src/components/Post/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PostDetails from "./PostDetails";
+
+const Post = ({ post }) => {
+  return (
+    <div className="postContainer">
+      <h3>Id: {post.id}</h3>
+      <h3>Title: {post.title}</h3>
+      <h3>Author: {post.author}</h3>
+    </div>
+  );
+};
+
+export default Post;
